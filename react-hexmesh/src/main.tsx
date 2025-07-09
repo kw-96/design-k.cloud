fb5a42fdb7729f74e52ee988ead0aebc079be66b
KW <530430624@qq.com>
2025-07-09 10:19:34 +0800

    Remove .gitignore file and implement URL redirection in main.tsx for RuleXcel and PyBegin projects, enhancing user experience with iframe integration.

--- react-hexmesh/src/main.tsx
+++ react-hexmesh/src/main.tsx
@@ -19,6 +19,27 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get('redirect');
+if (redirect) {
+  // 恢复地址栏为原始路径
+  window.history.replaceState(null, '', redirect);
+  if (redirect.toLowerCase().startsWith('/rulexcel')) {
+    document.body.innerHTML = '<iframe src="https://kw-96.github.io/RuleXcel/" style="position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;margin:0;padding:0;z-index:9999;" allowfullscreen></iframe>';
+    document.body.style.background = '#fff';
+    document.body.style.overflow = 'hidden';
+    document.documentElement.style.overflow = 'hidden';
+    throw new Error('Redirected to RuleXcel');
+  }
+  if (redirect.toLowerCase().startsWith('/pybegin')) {
+    document.body.innerHTML = '<iframe src="https://kw-96.github.io/PyBegin/" style="position:fixed;top:0;left:0;width:100vw;height:100vh;border:none;margin:0;padding:0;z-index:9999;" allowfullscreen></iframe>';
+    document.body.style.background = '#fff';
+    document.body.style.overflow = 'hidden';
+    document.documentElement.style.overflow = 'hidden';
+    throw new Error('Redirected to PyBegin');
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
